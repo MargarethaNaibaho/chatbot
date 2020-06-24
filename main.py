ff6557dc67e03d2fe5f962e0b8592fe583df4a29
MargarethaNaibaho <63283140+MargarethaNaibaho@users.noreply.github.com>
2020-06-24 18:06:07 +0700

    bisa4

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ def dataIndonesia():
     import requests
     source = requests.get('https://www.kompas.com/covid-19')
     soup = BeautifulSoup(source.text, 'lxml')
-
+    
     data = soup.find_all('div', {'class':'covid__box2'})
-
+    
     positif = data[0].text.split('i')[2].split(' ')[0]
     tambahan = data[0].i.text
     dirawat = data[1].text.split('t')[1].split(' ')[0]
     meninggal = data[2].text.split('l')[1].split(' ')[0]
     sembuh = data[3].text.split('h')[1].split(' ')[0]
-
+    
     tgl_Update = soup.find('div', {'class':'covid__header'}).span.text.split(':')[1].split(',')[0]
-
+    
     teks = "Data Covid-19 di Indonesia per" + tgl_Update + "\n"
     teks += "Positif : " + positif + "(" + tambahan + ")" + "\n"
     teks += "Dirawat : " + dirawat + "\n"
